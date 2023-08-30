74c7e232d84eeac754ea5df313e7272b165143f2
Andy Jenkins <andrewajenkins@outlook.com>
2023-08-30 16:54:04 -0700

    code highlighting theme changes with theme change

--- src/app/core/services/style/theme.service.ts
+++ src/app/core/services/style/theme.service.ts
@@ -10,11 +10,32 @@ export class ThemeService {
   }
 
   setDarkTheme(isDarkTheme: boolean) {
+    this.toggleTheme();
     this.darkTheme = isDarkTheme;
     document.body.setAttribute('data-theme', isDarkTheme ? 'dark' : 'light');
   }
 
   isDarkTheme() {
     return this.darkTheme;
+  }
+
+  toggleTheme() {
+    const stylesheets = document.styleSheets;
+    const githubStylesheet = Array.from(stylesheets).find(
+      (stylesheet: any) => stylesheet.href && stylesheet.href.includes('github'),
+    );
+    const darkStylesheet = Array.from(stylesheets).find(
+      (stylesheet: any) => stylesheet.href && stylesheet.href.includes('dark'),
+    );
+
+    if (githubStylesheet && darkStylesheet) {
+      if (githubStylesheet.disabled) {
+        githubStylesheet.disabled = false;
+        darkStylesheet.disabled = true;
+      } else {
+        githubStylesheet.disabled = true;
+        darkStylesheet.disabled = false;
+      }
+    }
   }
 }
