fbf4e0614c9289d4cd3638a495adeee6d4f9b9ff
Andy Jenkins <andrewajenkins@outlook.com>
2023-08-29 21:48:39 -0700

    table shows with dummy data and is light/dark themed

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule]
+  imports: [CommonModule, SharedModule, HomeRoutingModule, MatTableModule, NgFor, MatButtonModule, NgIf, MatIconModule],
 })
 export class HomeModule {}
