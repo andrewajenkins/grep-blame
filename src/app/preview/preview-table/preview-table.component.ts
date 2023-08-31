bb5ea0965dc54a3de67e3c2719c5bcc152cb8a20
Andy Jenkins <andrewajenkins@outlook.com>
2023-08-30 18:39:05 -0700

    fix side toolbar being parallel with other content - and other content taking full space

--- src/app/preview/preview-table/preview-table.component.ts
+++ src/app/preview/preview-table/preview-table.component.ts
@@ -9,4 +9,18 @@ import { RipGrepResult } from '../../../../app/commands/ripgrep';
 export class PreviewTableComponent {
   @Input() dataSource: RipGrepResult[] = [];
   displayedColumns = ['fileName', 'commit', 'blame', 'dateTime', 'lineNum'];
+  ngOnInit() {
+    const test = [];
+    for (let i = 0; i < 20; i++) {
+      test.push({
+        fileName: 'src/app/app.component.ts',
+        commit: 'commit 1',
+        blame: 'blame 1',
+        dateTime: 'dateTime 1',
+        lineNum: 'lineNum 1',
+        content: 'content 1',
+      });
+    }
+    this.dataSource = test;
+  }
 }
